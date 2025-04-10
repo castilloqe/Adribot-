8727fbe11049e8e2fe90d61ca0d161487de15fb0
Orlando <willzekoficial@gmail.com>
2025-04-10 17:15:22 -0600

    Actualizar fun-casino.js

--- plugins/fun-casino.js
+++ plugins/fun-casino.js
@@ -21,11 +21,11 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `*🌟 Estrellas*` + ' que deseas aportar contra' + ` *CrowBot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `*🌟 Estrellas*` + ' que deseas aportar contra' + ` *SonicBot-MF*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 if (user.estrellas >= count * 1) {
 user.estrellas -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *CrowBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌟 Estrellas`.trim(), m, rcanal)
+conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *SonicBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌟 Estrellas`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.estrellas += count * 2
 conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *CrowBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🌟 Estrellas`.trim(), m, rcanal)
