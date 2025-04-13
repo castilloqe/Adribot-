b75fe405884a0bd383ab2b95c20a6cbe41d63050
Orlando <willzekoficial@gmail.com>
2025-04-12 18:32:42 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         contextInfo: {
             externalAdReply: {
                 showAdAttribution: true,
-                title: 'һ᥆ᥣᥲ s᥆ᥡ ᥕіᥣᥣzᥱk-᥆𝖿ᥴ ᥱᥣ mᥱȷ᥆r',
+                title: 'һ᥆ᥣᥲ s᥆ᥡ Orlando-᥆𝖿ᥴ ᥱᥣ mᥱȷ᥆r',
                 body: dev,
                 thumbnailUrl: 'https://files.catbox.moe/wd3wwq.jpg',
                 sourceUrl: 'https://wa.me/522731590195?text=Vengo+Del+Comando+.owner',
