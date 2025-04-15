1f4c4112a6ae5c70a0af6ba6b91934035b397a2b
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-15 09:47:24 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -10,8 +10,13 @@ let moon = args[0];
 let force = await (await fetch (`https://moonforce-apiofc.vercel.app/api/download/tiktok?url=${moon}`)).json();
 
 let txt = `*Titulo:* force.results.title`
+let img = force.results.thumbnail;
+
+conn.sendFile(m.chat, img, 'MoonForce.jpg', txt, null, rcanal);
+
+conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m });
 }
 
-MF.command = ['google', 'test'];
+MF.command = ['test'];
 
 export default MF;
