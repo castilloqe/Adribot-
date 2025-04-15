76dbbb7624d542ab49684443ccf4f2de8c84961b
Orlando <willzekoficial@gmail.com>
2025-04-15 07:27:08 -0600

    Actualizar main-menuto.js

--- plugins/main-menuto.js
+++ plugins/main-menuto.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'tools': 'MenuTools',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = 'https://files.catbox.moe/e6hef7.jpg'
 
   try {
           // DEFAULT MENU
