0d651e2a77f05604cec96931ee269de76f604030
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-15 09:56:46 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,21 +1,21 @@
-// Google By WillZek >> https://github.com/WillZek
-
 import fetch from 'node-fetch';
 
-let MF = async(m, { conn, usedPrefix, command, args }) => {
+let MF = async (m, { conn, usedPrefix, command, args }) => {
 
-if (!args[0]) return m.reply('🌃 Ingrese Para Buscar En Google');
+if (!args[0]) return m.reply('🌃 Ingrese una URL de TikTok');
 
 let moon = args[0];
-let force = await (await fetch (`https://moonforce-apiofc.vercel.app/api/download/tiktok?url=${moon}`)).json();
+let force = await (await fetch(`https://moonforce-apiofc.vercel.app/api/download/tiktok?url=${moon}`)).json();
 
-let txt = `*Titulo:* force.results.title`
-let vid = force.results.video.no_watermark;
+if (!force.result) return m.reply('❌ Error al obtener los datos');
 
-conn.sendFile(m.chat, img, 'MoonForce.jpg', txt, null, rcanal);
+let { title, video, thumbnail } = force.result;
+let txt = `*Título:* ${title}`;
+let vid = video.no_watermark;
 
-conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m });
-}
+await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m); // miniatura
+await conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m });
+};
 
 MF.command = ['test'];
 
