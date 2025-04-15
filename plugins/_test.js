c0a1c810bd63ba2102dc7100042bb3b00c51f24f
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-15 09:41:31 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -7,7 +7,7 @@ let MF = async(m, { conn, usedPrefix, command, args }) => {
 if (!args[0]) return m.reply('🌃 Ingrese Para Buscar En Google');
 
 let moon = args[0];
-let force = `https://api.siputzx.my.id/api/tools/ssweb?url=https://www.google.com/search?q=${moon}`;
+let force = `https://moonforce-apiofc.vercel.app/api/download/tiktok?url=${moon}`;
 
 let txt = `*Resultados De: ${args[0]}`;
 
