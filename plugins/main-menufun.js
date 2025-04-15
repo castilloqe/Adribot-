4d8ac0779cee02a993e9136f8e3b7870f4e3830f
Orlando <willzekoficial@gmail.com>
2025-04-15 07:16:42 -0600

    Actualizar main-menufun.js

--- plugins/main-menufun.js
+++ plugins/main-menufun.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'fun': 'Diversion',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = 'https://files.catbox.moe/hso0pz.jpg'
 
   try {
           // DEFAULT MENU
