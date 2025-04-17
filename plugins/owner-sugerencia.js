89cfc51c732def358527a6c31d93a99d6fdf7759
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-16 18:32:15 -0600

    Actualizar owner-sugerencia.js

--- plugins/owner-sugerencia.js
+++ plugins/owner-sugerencia.js
@@ -3,13 +3,15 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.length < 5) return conn.reply(m.chat, '🌠 La sugerencia debe ser más de 5 caracteres.', m)
     if (text.length > 1000) return conn.reply(m.chat, '🌠 Máximo de la sugerencia es de 1000 caracteres.', m)
     
-    const teks = `🌠 Sugerencia de nuevo comando del usuario *${m.sender}*
+    const teks = `🌠 Sugerencia de nuevo comando del usuario *${conn.getName(m.sender)}*
 
 🛡️ Han sugerido un comando:
 > ${text}`
     
     const groupChatId = '120363395340241509@g.us';
-    await conn.reply(groupChatId, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+/*    await conn.reply(groupChatId, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+*/
+     await conn.reply(groupChatId, m.quoted ? teks + m.quoted.text : teks, m, { mentions: [m.sender] })
 
     m.reply('🌠 La sugerencia se envió al Staff De Sonicbot.')
 }
