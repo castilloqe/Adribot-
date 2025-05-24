d5e7254fb22eaaaf1cddd733bca8e5d4d7db194c
Orlando <willzekoficial@gmail.com>
2025-05-23 22:18:02 -0600

    Actualizar tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -1,36 +1,53 @@
- import axios from "axios";
-import uploadImage from "../lib/uploadImage.js";
-
-const handler = async (m, { conn }) => {
-  try {
-    const q = m.quoted || m;
-    const mime = (q.msg || q).mimetype || q.mediaType || "";
-    if (!mime.startsWith("image/")) {
-      return conn.reply(m.chat, " Responde a una *Imagen.*", m);
-    }
-
-    await m.react("🕓");
-    const imgBuffer = await q.download?.();
-    const urlSubida = await uploadImage(imgBuffer);
-    const upscaledBuffer = await getUpscaledImage(urlSubida);
-
-    await conn.sendFile(m.chat, upscaledBuffer, "upscaled.jpg", "*Aquí tienes tu imagen mejorada ฅ^•ﻌ•^ฅ*", m);
-    await m.react("✅");
-  } catch (e) {
-    console.error("Error:", e);
-    await m.react("✖️");
-    conn.reply(m.chat, "Ocurrió un error al mejorar la imagen.", m);
-  }
+ import FormData from "form-data";
+import Jimp from "jimp";
+const handler = async (m, {conn, usedPrefix, command}) => {
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) return m.reply(`🚩 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
+  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`🍂 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
+  conn.reply(m.chat, '🚩 Mejorando la calidad de la imagen....', m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: wm,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: channel }}})
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
+ } catch {
+ return m.reply("🚩 Ocurrió un error");
+ }
 };
-
-handler.help = ["hd"]  
-handler.tags = ["tools"]  
-handler.command = ["remini", "hd", "enhance"]  
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.group = true;
 handler.register = true
+handler.command = ["remini", "hd", "enhance"];
 export default handler;
 
-async function getUpscaledImage(imageUrl) {
-  const apiUrl = `https://api.siputzx.my.id/api/iloveimg/upscale?image=${encodeURIComponent(imageUrl)}`;
-  const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
-  return Buffer.from(response.data);
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
+    }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
 }
